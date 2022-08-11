9ae5b4899d266298da19a1f52241427555ba1575
Wijayaac <wijayaac25@gmail.com>
2022-08-11 23:06:56 +0800

    Update : object destructure, spread

--- data-type/object.js
+++ data-type/object.js
@@ -9,3 +9,28 @@ const student = {
 };
 
 console.log(student.bestFriend.age);
+
+const car = {
+  brand: "Toyota",
+  name: "Avanza",
+  year: 2015,
+};
+
+const ownerToyota = {
+  name: "Wijaya",
+  address: {
+    city: "Denpasar",
+    street: "Ubung 123",
+  },
+};
+
+const carWithOwner = {
+  ...car,
+  ownerToyota,
+};
+
+const {
+  ownerToyota: { name: ownerName },
+} = carWithOwner;
+
+console.log(ownerName, ownerToyota);
