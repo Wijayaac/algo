e52cfd4402c04da867f5b6b814ed4e15f4344c5e
Wijayaac <wijayaac25@gmail.com>
2022-08-11 22:34:38 +0800

    Add : first excersises

---
+++ data-type/excersises-1.js
@@ -0,0 +1,9 @@
+// convert lowercase string input to capital first letter
+
+// wijaya => Wijaya
+
+const myName = "wijaya";
+
+let capitalize = myName.charAt(0).toUpperCase() + myName.slice(1);
+
+console.log(capitalize);
